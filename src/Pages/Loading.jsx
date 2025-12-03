3e5a60feb11542b973c90a512b046225a82d70bd
Akash Deb Nath <akash.dns12345@gmail.com>
2025-12-03 17:05:10 +0600

    updated latest news

--- src/Pages/Loading.jsx
+++ src/Pages/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
     return (
         <div className='flex justify-center items-center p-20'>
-            <span class="loading loading-spinner text-warning"></span>
+            <span className="loading loading-spinner text-warning"></span>
         </div>
     );
 };
--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Marquee from 'react-fast-marquee';
 
 const LatestNews = () => {
+    const [news, setNews] = useState([]);
+    useEffect(() => {
+        fetch('/news.json')
+            .then(res => res.json())
+            .then(data => setNews(data))
+    }, [])
+    const breakingNews = news.filter(singleNews => singleNews.category_id == "2");
+    // console.log(breakingNews);
     return (
         <div className='flex items-center gap-5 bg-base-200 p-3'>
             <p className='text-base-100 bg-secondary px-3 py-2'>Latest</p>
             <Marquee className='flex gap-3' pauseOnHover={true} speed={50}>
-                <p className='font-bold'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, rem?</p>
-                <p className='font-bold'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, rem?</p>
-                <p className='font-bold'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, rem?</p>
+                {
+                    breakingNews.map(singleBreakingNews => <p className='mx-3'>{singleBreakingNews.title}.</p>)
+                }
             </Marquee>
         </div>
     );
